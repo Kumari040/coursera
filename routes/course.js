7d2fa12773003e63362c28dd9cc1daa4293afa83
Kumari040 <kms745362@gmail.com>
2025-05-10 17:08:12 +0530

    course route is updated.

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,24 @@
 const {Router}=require("express");
 const courseRouter=Router();
 const {courseModel,purchaseModel,}=require("../db");
-courseRouter.post("/purchase",function(req,res){
+const {userMiddleware}=require("../middleware/user")
+courseRouter.post("/purchase",userMiddleware,async function(req,res){
+    const userId=req.userId;
+    const courseId=req.body;
+    await purchaseModel.create({
+        userId,
+        courseId
+    });
+    res.json({
+        message:"You have successfully bought the course."
+    });
 
 });
-courseRouter.post("/preview",function(req,res){
-
+courseRouter.post("/preview",async function(req,res){
+    const courses=await courseModel.find({});
+    res.json({
+        courses
+    });
 });
 module.exports={
     courseRouter
